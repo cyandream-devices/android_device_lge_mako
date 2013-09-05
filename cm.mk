2cef7f602b0bc8c621bffaff8e3825137c08ed1e
yanniks <kontakt@yanniks.de>
2013-09-05 21:59:24 +0200

    CyanDream vendor

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyandream/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
