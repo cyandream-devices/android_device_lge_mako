156edac06c85295e1e3c50e691d4b05f2558fc51
yanniks <kontakt@yanniks.de>
2013-09-15 13:34:31 +0200

    renamed cm.mk to cd.mk

--- cd.mk
+++ cd.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cyandream/config/gsm.mk)
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Inherit some common CM stuff.
+# Inherit some common CD stuff.
 $(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Enhanced NFC
@@ -16,7 +16,7 @@ $(call inherit-product, device/lge/mako/full_mako.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := cm_mako
+PRODUCT_NAME := cd_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
